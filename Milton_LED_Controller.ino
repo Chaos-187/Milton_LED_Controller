0447b279952021e05e5440d0d665dd35fa92b2f3
Kyle Harris <jkcomputers_554@hotmail.com>
2018-02-24 21:19:18 +0000

    first commit

---
+++ Milton_LED_Controller.ino
@@ -0,0 +1,26 @@
+#include "FastLED.h"
+
+FASTLED_USING_NAMESPACE
+
+// Author : Kyle Harris
+// Date Created : 24/02/2018
+// Description : Test App for LED's in the milton
+// Version : 1.00
+// Enviroment : Dev
+
+
+#define DATA_PIN    3
+//#define CLK_PIN   4
+#define LED_TYPE    WS2812B
+#define COLOR_ORDER GRB
+#define NUM_LEDS    64
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
